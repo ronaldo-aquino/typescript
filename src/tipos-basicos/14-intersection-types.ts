65a0575eefb61fdc4b328877827d0b3a57ca1918
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-10 17:35:41 -0300

    Intersection types

---
+++ src/tipos-basicos/14-intersection-types.ts
@@ -0,0 +1,21 @@
+type TemNome = { nome: string };
+type TemSobrenome = { sobrenome: string };
+type TemIdade = { idade: number };
+
+type Pessoa = TemNome & TemSobrenome & TemIdade;
+
+type AB = 'A' | 'B';
+type AC = 'A' | 'C';
+type AD = 'D' | 'A';
+type Intersecao = AB & AC & AD;
+
+const pessoa: Pessoa = {
+    idade: 26,
+    nome: 'Ronaldo',
+    sobrenome: 'Aquino',
+};
+
+console.log(pessoa);
+
+// Module mode
+export { pessoa };

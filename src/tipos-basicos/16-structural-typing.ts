58c0fb321a2ebdb54ce4976c54ac50c90fbca94b
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-10 18:27:43 -0300

    Structural Typing

---
+++ src/tipos-basicos/16-structural-typing.ts
@@ -0,0 +1,17 @@
+type VerifyUserFn = (user: User, sentValue: User) => boolean;
+
+type User = { username: string; password: string };
+
+const verifyUser: VerifyUserFn = (user, sentValue) => {
+    return (
+        user.username === sentValue.username &&
+        user.password === sentValue.password
+    );
+};
+
+const bdUser = { username: 'Ronaldo', password: '123456' };
+const sentUser = { username: 'Ronaldo', password: '123456' };
+
+const loggedIn = verifyUser(bdUser, sentUser);
+
+console.log(loggedIn);

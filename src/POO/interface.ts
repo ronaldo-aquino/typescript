cb563b30d5d9ef55fdd2fae96608aa4dbe486a25
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-12 22:47:54 -0300

    Inteface

---
+++ src/POO/interface.ts
@@ -0,0 +1,24 @@
+interface TipoNome {
+    nome: string;
+}
+
+interface TipoSobrenome {
+    sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+    nomeCompleto(): string;
+}
+
+interface TipoPessoa extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+export class Pessoa implements TipoPessoa {
+    constructor(public nome: string, public sobrenome: string) {}
+
+    nomeCompleto(): string {
+        return `${this.nome} ${this.sobrenome}`;
+    }
+}
+
+const pessoa = new Pessoa('Ronaldo', 'Aquino');
+console.log(pessoa.nomeCompleto());

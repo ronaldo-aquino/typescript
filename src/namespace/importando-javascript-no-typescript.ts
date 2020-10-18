2f3dddfad8dc9a4842786315d1d7e45271c7d954
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-18 20:06:14 -0300

    Importando arquivos javascript no typescript

---
+++ src/namespace/importando-javascript-no-typescript.ts
@@ -0,0 +1,8 @@
+import { soma } from './soma';
+import { somar } from './somar';
+
+// const resultado = soma(10, 2) as number;
+// console.log(resultado);
+
+const sum = somar(10, 15);
+console.log(sum);

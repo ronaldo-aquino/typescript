81d801bdb2f09eaf0c0c4c5d6c4a94253be00c2b
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-18 19:38:02 -0300

    Criando o primeiro namespace

---
+++ src/namespace/criando-um-namespace.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+namespace MeuNamespace {
+    export const nome = 'Ronaldo';
+
+    export class PessoaDoNamespace {
+        constructor(public nome: string) {}
+    }
+
+    const pessoa = new PessoaDoNamespace(nome);
+    console.log(pessoa);
+}
+
+const pessoaDoNamespace = new MeuNamespace.PessoaDoNamespace(MeuNamespace.nome);
+console.log(pessoaDoNamespace);
+
+export default 1;
